7a01feef69ca0f92f53888552c022b208400d902
HuiZHANG <13162199796@163.com>
2019-01-17 20:22:04 +0800

    merge_txt.py
    merge many txt files with simple python code

---
+++ merge_txt.py
@@ -0,0 +1,6 @@
+import glob 
+read_files = glob.glob("*.features") 
+with open("result.txt", "wb") as outfile: 
+  for f in read_files: 
+    with open(f, "rb") as infile: 
+      outfile.write(infile.read())
